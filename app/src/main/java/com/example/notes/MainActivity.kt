75029df117b1ae503290fbbce37e566c108f0608
ruantees <ruant142@gmail.com>
2023-11-11 17:55:41 +0200

    Populated the recycler

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var notes: ArrayList<Note>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvContacts = findViewById<View>(R.id.rvNotes) as RecyclerView
+        notes = Note.createNotesList(20)
+
+        val adapter = NotesAdapter(notes)
+        rvContacts.adapter = adapter
+        rvContacts.layoutManager = LinearLayoutManager(this)
     }
 }
